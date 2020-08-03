514bcec2bccc7776fdb63b906bee777ed1d74f20
yu-lin42 <yu-lin@student.wethinkcode.co.za>
2020-08-03 17:02:32 +0200

    It is time

--- dbc.js
+++ dbc.js
@@ -50,7 +50,7 @@ connection.connect((err) => {
 						+ `verified INT(10) NOT NULL,`
 						+ `token VARCHAR(200) NOT NULL,`
 						+ `password VARCHAR(200) NOT NULL,`
-						+ `resetToken VARCHAR(200) NOT NULL,`
+						+ `resetToken VARCHAR(200),`
 						// + `allowReset INT(10) NOT NULL,`
 						+ `firstLogin INT(10),`
 						+ `age INT(10),`

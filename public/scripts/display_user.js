fafa94106e83a4301a7d94bd828c07d04cae0c30
yu-lin42 <yu-lin@student.wethinkcode.co.za>
2020-08-04 01:50:38 +0200

    cleaning up some of the comments

--- public/scripts/display_user.js
+++ public/scripts/display_user.js
@@ -6,28 +6,24 @@ function plusSlides(n) {
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+	showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("card");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-//   console.log(n);
-  slides[slideIndex-1].style.display = "block";
-  var modal = document.getElementById("myModal");
+	console.log('Hello');
+	console.log(n);
+	var i;
+	var slides = document.getElementsByClassName("card");
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[slideIndex-1].style.display = "block";
+
+	var modal = document.getElementById("myModal");
 	modal.onclick = function() {
 		modal.style.display = "block";
 	}
-	var span = document.getElementsByClassName("close")[0];
-	
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() { 
-		// Get the <span> element that closes the modal
-	  modal.style.display = "none";
+	if (n === slides.length) {
+		console.log('no more');
 	}
 }

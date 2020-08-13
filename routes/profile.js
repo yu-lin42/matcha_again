32e2f45d501145af1e2effa1c5e851f9d31d3628
yu-lin42 <yu-lin@student.wethinkcode.co.za>
2020-08-13 20:48:42 +0200

    Hopefully its done

--- routes/profile.js
+++ routes/profile.js
@@ -1,18 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var connection = require('../dbc').connection;
+const mysql = require('../test');
 
 router.get('/', (req, res, next) => {
 	// check if firstTime is a thing
 	console.log(req.session.userID);
-	function getNotifications() {
-		console.log('Testing');
-	}
-	setInterval(getNotifications, 1000);
+	// function getNotifications() {
+	// 	console.log('Testing');
+	// }
+	// setInterval(getNotifications, 1000);
 	let checkFirstLoginArray = [req.session.userID];
 	let selectValues = `username, firstLogin, interest1, interest2, interest3, interest4, 
 	sexualOrientation, name, surname, age, gender, agePreference, biography, city, viewedBy, rating`
 	let checkFirstLoginQuery = `SELECT ${selectValues} FROM users WHERE id = ?`;
+	
+	async function viewMatched() {
+		const connection = await mysql.connection();
+		try {
+			let username = await connection.query(`SELECT username FROM users WHERE id = ?`, [req.session.userID]);
+			let matchedData = await connection.query(`SELECT usernameOfLiked FROM connections WHERE username = ? AND matched = ?`, [username[0].username, 1]);
+			// console.log(matchedData);
+			return(matchedData);
+		}
+		catch (err) {
+			await connection.query('ROLLBACK');
+			throw err;
+		}
+		finally {
+			connection.release();
+		}
+	}
+	
 	connection.query(checkFirstLoginQuery, checkFirstLoginArray, (err, results) => {
 		if (err) {
 			throw err;
@@ -35,6 +54,7 @@ router.get('/', (req, res, next) => {
 			let viewedBy = (results[0].viewedBy).split(",");
 			let rating = results[0].rating;
 			let city = results[0].city;
+			let foundMatched = [];
 
 			if (results[0].firstLogin === 0) {
 				if (results[0].interest1 !== null) {
@@ -50,24 +70,33 @@ router.get('/', (req, res, next) => {
 				if (results[0].interest4 !== null) {
 					priorityArray[0].interest4 = results[0].interest4;
 				}
+				viewMatched().then((matchedObject) => {
+					matchedObject.forEach((matchedData) => {
+						foundMatched.push(matchedData);
+					})
+					// console.log(foundMatched);
 				
-				// console.log('Are you there?');
-				res.render('profile', {
-					title: 'Profile',
-					loginStatus : req.session.userID ? 'logged_in' : 'logged_out',
-					firstTimeSetup : 1,
-					priorityArray : priorityArray,
-					username : username,
-					name : name,
-					surname : surname,
-					age : age,
-					gender : gender,
-					sexualOrientation : sexualOrientation,
-					agePreference : agePreference,
-					biography : biography,
-					city : city,
-					viewedBy : viewedBy,
-					rating : rating
+					res.render('profile', {
+						title: 'Profile',
+						loginStatus : req.session.userID ? 'logged_in' : 'logged_out',
+						firstTimeSetup : 1,
+						priorityArray : priorityArray,
+						username : username,
+						name : name,
+						surname : surname,
+						age : age,
+						gender : gender,
+						sexualOrientation : sexualOrientation,
+						agePreference : agePreference,
+						biography : biography,
+						city : city,
+						viewedBy : viewedBy,
+						rating : rating,
+						matched : foundMatched
+					});
+				})
+				.catch((err) => {
+					console.log(err);
 				});
 			}
 			else if (results[0].firstLogin === 1) {
@@ -83,3 +112,6 @@ router.get('/', (req, res, next) => {
 });
 
 module.exports = router;
+
+
+

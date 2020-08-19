2b63a4b5f4d456ca283156600e514ec95a61a251
yu-lin42 <yu-lin@student.wethinkcode.co.za>
2020-08-19 19:08:36 +0200

    fixing things

--- routes/setup_profile.js
+++ routes/setup_profile.js
@@ -14,7 +14,7 @@ router.get('/', (req, res, next) => {
 			res.render('setupProfile', {
 				title : `Setup Profile`,
 				loginStatus: req.session.userID ? 'logged_in' : 'logged_out',
-				userdata : results
+				errorMessages : []
 			});
 		}
 	});
@@ -26,16 +26,73 @@ router.post('/check', (req, res, next) => {
 	sexualOrientation = req.body.sexualOrientation;
 	agePreference = req.body.agePreference
 	id = req.session.userID;
-	let queryStuff = [interests[0], interests[1], interests[2], interests[3], `0`, sexualOrientation, agePreference, biography, req.session.userID];
-	let interestQuery = `UPDATE users SET interest1 = ?, interest2 = ?, interest3 = ?, interest4 = ?, firstLogin = ?, sexualOrientation = ?, agePreference = ?, biography = ? WHERE id = ?`;
-	connection.query(interestQuery, queryStuff, (err, results)  => {
-		if (err){
-			console.log(err);
-			throw(err);
-		}
-		else{
-			res.redirect('/users');
-		}
-	});
+	console.log(biography);
+	let bioErrors = {
+		fieldLength : ``,
+		noErrors : `No`
+	};
+	let interestErrors = {
+		noSelection : ``,
+		numberOfErrors : ``,
+		noErrors : `No`
+	};
+	let sexualOrientationErrors = {
+		noSelection : ``,
+		noErrors : `No`
+	};
+	let agePreferenceErrors = {
+		noSelection : ``,
+		noErrors : `No`
+	};
+	
+	if (biography.length === 0) {
+		bioErrors[`fieldLength`] = `This field may not be empty.`;
+	} else {
+		bioErrors[`noErrors`] = `Yes`;
+	}
+	if (interests === undefined) {
+		interestErrors[`noSelection`] = `This field cannot be empty.`;
+	} else if (interests.length !== 4) {
+		interestErrors[`numberOfErrors`] = `You must only pick 4 topics.`;
+	} else {
+		interestErrors[`noErrors`] = `Yes`;
+	}
+	if (sexualOrientation === undefined) {
+		sexualOrientationErrors[`noSelection`] = `This field cannot be empty.`;
+	} else {
+		sexualOrientationErrors[`noErrors`] = `Yes`;
+	}
+	if (agePreferenceErrors === undefined) {
+		agePreferenceErrors[`noSelection`] = `This field cannot be empty`;
+	} else {
+		agePreferenceErrors[`noErrors`] = `Yes`;
+	}
+	if (bioErrors.noErrors === `No` || interestErrors.noErrors === `No` || sexualOrientationErrors.noErrors === `No` || agePreferenceErrors.noErrors === `No`) {
+		let errors = [
+			bioErrors,
+			sexualOrientationErrors,
+			agePreferenceErrors,
+			interestErrors
+		];
+		console.log(errors);
+		res.render(`setupProfile`, {
+			title : `Setup Profile`,
+			loginStatus : req.session.userID ? 'logged_in' : 'logged_out',
+			errorMessages : errors
+		});
+	}
+	else {
+		let queryStuff = [interests[0], interests[1], interests[2], interests[3], `0`, sexualOrientation, agePreference, biography, req.session.userID];
+		let interestQuery = `UPDATE users SET interest1 = ?, interest2 = ?, interest3 = ?, interest4 = ?, firstLogin = ?, sexualOrientation = ?, agePreference = ?, biography = ? WHERE id = ?`;
+		connection.query(interestQuery, queryStuff, (err, results)  => {
+			if (err){
+				console.log(err);
+				throw(err);
+			}
+			else {
+				res.redirect('/users');
+			}
+		});
+	}
 });
 module.exports = router;
